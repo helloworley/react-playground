5ac9c60ce4b7b5fa183d6b7ce8a908562e51e07e
Josh Worley <helloworley@gmail.com>
2020-12-22 12:04:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
+import './styles/App.css';
+import Contact from './components/authorization-form/Contact';
+import ColorChanger from './components/color-changer/ColorChanger';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Josh's React Playground</h2>
       </header>
+      <ColorChanger />
+      <Contact />
     </div>
   );
 }
